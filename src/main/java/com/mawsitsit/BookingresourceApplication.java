1b2af002bba3d2683b42b871c619c89849053a9d
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-06-08 11:27:02 +0200

    Init project

---
+++ src/main/java/com/mawsitsit/BookingresourceApplication.java
@@ -0,0 +1,12 @@
+package com.mawsitsit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingresourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingresourceApplication.class, args);
+	}
+}
