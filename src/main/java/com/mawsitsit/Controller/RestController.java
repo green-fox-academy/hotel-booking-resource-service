1ed7029811b5259e1cdac64bde935d8e7253ace9
Mate Matiovics <dontirikk@gmail.com>
2017-06-09 14:47:45 +0200

    remove /main endpoint

--- src/main/java/com/mawsitsit/Controller/RestController.java
+++ src/main/java/com/mawsitsit/Controller/RestController.java
@@ -4,7 +4,6 @@ import com.mawsitsit.Model.Status;
 import com.mawsitsit.Service.StatusChecker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
@@ -16,10 +15,5 @@ public class RestController {
   @GetMapping("/hearthbeat")
   public Status checkApp () {
     return statusChecker.serviceStatus();
-  }
-
-  @RequestMapping({"/", "/index"})
-  public String main() {
-    return "Hello World!";
   }
 }
