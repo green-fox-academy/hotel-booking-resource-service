2ceb6fd1064dd1e6f3db39fb941874b923785e1d
Mate Matiovics <dontirikk@gmail.com>
2017-06-11 16:45:14 +0200

    add flyway dependency and migration resource file

--- src/main/java/com/mawsitsit/BookingresourceApplication.java
+++ src/main/java/com/mawsitsit/BookingresourceApplication.java
@@ -1,25 +1,12 @@
 package com.mawsitsit;
 
-import com.mawsitsit.Model.Hearthbeat;
-import com.mawsitsit.Repository.HearthbeatRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookingresourceApplication implements CommandLineRunner {
-
-	@Autowired
-	private
-	HearthbeatRepository hearthbeatRepo;
+public class BookingresourceApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingresourceApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		hearthbeatRepo.save(new Hearthbeat());
 	}
 }
