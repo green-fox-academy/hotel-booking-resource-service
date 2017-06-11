58e813653b7cfcd4786807ca35280858cbb34eb0
Mate Matiovics <dontirikk@gmail.com>
2017-06-11 13:36:33 +0200

    add temporary new endpoint for deployement testing

--- src/main/java/com/mawsitsit/Controller/RestController.java
+++ src/main/java/com/mawsitsit/Controller/RestController.java
@@ -1,6 +1,8 @@
 package com.mawsitsit.Controller;
 
+import com.mawsitsit.Model.Hearthbeat;
 import com.mawsitsit.Model.Status;
+import com.mawsitsit.Repository.HearthbeatRepository;
 import com.mawsitsit.Service.StatusChecker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ public class RestController {
   private
   StatusChecker statusChecker;
 
+  @Autowired
+  private HearthbeatRepository hearthbeatRepo;
+
   @GetMapping("/hearthbeat")
   public Status checkApp () {
     return statusChecker.serviceStatus();
@@ -21,5 +26,11 @@ public class RestController {
   @RequestMapping({"/", "/index"})
   public String main() {
     return "Hello World!";
+  }
+
+  @RequestMapping("/add")
+  public String add() {
+    hearthbeatRepo.save(new Hearthbeat());
+    return "ok";
   }
 }
