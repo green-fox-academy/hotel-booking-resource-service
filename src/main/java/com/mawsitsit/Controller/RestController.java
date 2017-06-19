1e6f47b6edb18ae24083b1a70f4b61460798d58e
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-06-19 14:53:18 +0200

    log

--- src/main/java/com/mawsitsit/Controller/RestController.java
+++ src/main/java/com/mawsitsit/Controller/RestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
