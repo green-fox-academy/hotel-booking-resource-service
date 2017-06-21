f85fda4daeeee91722db6daf723f586c1e91a7ef
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-06-21 16:25:29 +0200

    Refactor HotelListingService createList()

--- src/main/java/com/mawsitsit/Service/HotelListingService.java
+++ src/main/java/com/mawsitsit/Service/HotelListingService.java
@@ -20,13 +20,17 @@ public class HotelListingService {
   @Autowired
   private HotelRepository hotelRepository;
 
-  public HotelList createList(HttpServletRequest request, Pageable pageable){
+  public HotelList createList(HttpServletRequest request, Pageable pageable) {
     Page page = hotelRepository.findAll(pageable);
     List<Hotel> hotels = page.getContent();
     List hotelContainers = new ArrayList();
-    for(Hotel hotel : hotels) {
+    for (Hotel hotel : hotels) {
       hotelContainers.add(new HotelContainer("hotel", hotel.getId(), hotel));
     }
+    return new HotelList(linkBuilder(request, page), hotelContainers);
+  }
+
+  public Links linkBuilder(HttpServletRequest request, Page page) {
     Links links = new Links();
     links.setSelf(request.getRequestURL().toString());
     if (page.hasPrevious()) {
@@ -36,6 +40,6 @@ public class HotelListingService {
       links.setNext(request.getRequestURL().toString() + "?page=" + (page.getNumber() + 1));
       links.setLast(request.getRequestURL().toString() + "?page=" + page.getTotalPages());
     }
-    return new HotelList(links,hotelContainers);
+    return links;
   }
 }
