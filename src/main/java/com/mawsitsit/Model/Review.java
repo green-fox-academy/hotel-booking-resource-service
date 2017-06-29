7dfc6dc5f7e2ae7f3985034ccc71d765d640fbdd
Mate Matiovics <dontirikk@gmail.com>
2017-06-29 17:28:48 +0200

    create review class

---
+++ src/main/java/com/mawsitsit/Model/Review.java
@@ -0,0 +1,29 @@
+package com.mawsitsit.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Review {
+  @JsonIgnore
+  @Id
+  @GeneratedValue (strategy = GenerationType.AUTO)
+  private Long id;
+  @NotNull
+  private Integer rating;
+  @NotNull
+  private String dateTime = ZonedDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ssZ"));
+  @NotNull
+  private String description;
+}
+
