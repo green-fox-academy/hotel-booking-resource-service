d2654c0bd7a0f113b5d91339be652862cd4299b8
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-07-18 13:43:48 +0200

    fix test for average rating

--- src/test/java/com/mawsitsit/Controller/RESTControllerTest_withH2.java
+++ src/test/java/com/mawsitsit/Controller/RESTControllerTest_withH2.java
@@ -84,11 +84,15 @@ public class RESTControllerTest_withH2 {
     review2.setRating(2);
     Review review3 = initReview();
     review3.setHotel(hotelRepository.findOne(1L));
-    review3.setRating(2);
+    review3.setRating(3);
     review3.setDescription("Bad");
+    Review review4 = initReview();
+    review4.setRating(2);
+    review4.setHotel(hotelRepository.findOne(1L));
     reviewRepository.save(review1);
     reviewRepository.save(review2);
     reviewRepository.save(review3);
+    reviewRepository.save(review4);
   }
 
   @Test
@@ -139,8 +143,10 @@ public class RESTControllerTest_withH2 {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.data").exists())
             .andExpect(jsonPath("$.data.id").value(1))
+            .andExpect(jsonPath("$.data.attributes.average_rating").value(2.0))
             .andExpect(jsonPath("$.relationships.data[0].id").value(1))
-            .andExpect(jsonPath("$.included[0].attributes.rating").value(1));
+            .andExpect(jsonPath("$.included[0].attributes.rating").value(1))
+            .andExpect(jsonPath("$.included[1].attributes.rating").exists());
   }
 
   @Test
